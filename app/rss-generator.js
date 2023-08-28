f91f09e260869ff20441759d3fbd7b81841903f4
Fletcher Alderton <109054210+Fletcher-Alderton@users.noreply.github.com>
2023-08-28 03:02:16 +0000

    hopefully fixed image element

--- app/rss-generator.js
+++ app/rss-generator.js
@@ -57,31 +57,49 @@ async function fetchAndRefresh(sessionId) {
     }
 }
 
+
+
 // Function to update the RSS feed with fetched bookmark data and save as XML file
-function updateRSSFeed(bookmarks) {
+async function updateRSSFeed(bookmarks) {
     const root = create({ version: '1.0', encoding: 'UTF-8' }).ele('rss').att('version', '2.0');
-    root.att('xmlns:content', 'http://purl.org/rss/1.0/modules/content/'); // Declare the content namespace
+    root.att('xmlns:content', 'http://purl.org/rss/1.0/modules/content/');
 
     const channel = root.ele('channel');
-    channel.ele('title').txt('Shiori'); // Replace with your actual feed title
-    channel.ele('link').txt('https://fletcheralderton.com'); // Replace with your actual feed link
-    channel.ele('description').txt('My Shiori bookmarks'); // Replace with your actual feed description
+    channel.ele('title').txt('Shiori');
+    channel.ele('link').txt('https://fletcheralderton.com');
+    channel.ele('description').txt('My Shiori bookmarks');
+    // Add self-reference link using Atom namespace
+    channel.ele('atom:link').att('href', 'https://shiori2rss.rehctelf.com').att('rel', 'self').att('type', 'application/rss+xml');
 
-    bookmarks.forEach(bookmark => {
+    for (const bookmark of bookmarks) {
+        // Function to fetch the length of the image
+        async function getImageLength(imageURL) {
+            try {
+                const response = await fetch(imageURL);
+                if (response.ok) {
+                    return response.headers.get('content-length');
+                }
+            } catch (error) {
+                console.error('Error fetching image:', error);
+            }
+            return null;
+        }
+
         const item = channel.ele('item');
         item.ele('title').txt(bookmark.title);
         item.ele('description').txt(bookmark.excerpt);
-        item.ele('pubDate').txt(new Date(bookmark.modified).toUTCString()); // Format the date
-        item.ele('link').txt(encodeURI(bookmark.url)); // Encode the URL
-        item.ele('guid').txt(bookmark.url); // Adding a guid element as per RSS specification
-        item.ele('content:encoded').txt(`<a href="${encodeURI(bookmark.url)}">Read More</a>`); // Encode the URL
-        // adds a preview image if availabe
+        item.ele('pubDate').txt(new Date(bookmark.modified).toUTCString());
+        item.ele('link').txt(encodeURI(bookmark.url));
+        item.ele('guid').txt(bookmark.url);
+        item.ele('content:encoded').txt(`<a href="${encodeURI(bookmark.url)}">Read More</a>`);
+
         if (bookmark.imageURL) {
-            item.ele('enclosure')
-                .att('url', `${apiBaseUrl}${bookmark.imageURL}`)
-                .att('type', 'image/jpeg'); // May need to get the image type dynamicly
+            const imageLength = await getImageLength(`${apiBaseUrl}${bookmark.imageURL}`);
+            if (imageLength) {
+                item.ele('enclosure').att('url', `${apiBaseUrl}${bookmark.imageURL}`).att('type', 'image/jpeg').att('length', imageLength);
+            }
         }
-    });
+    }
 
     const rssFeed = root.end({ prettyPrint: true });
 
